8b8efcc46c23dbf6e12cc12ed4aca25cc11a37ad
Emil Gustavsen <emilgustavsen@gmail.com>
2018-11-06 13:49:43 +0100

    removed unnecessary files

--- js/script.js
+++ js/script.js
@@ -5,19 +5,70 @@ FSJS project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
+// Added quotes 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
-***/
-
-
-
+let quotes = [
+{
+  quote: '“Confidence is ignorance. If you\'re feeling cocky, it\'s because there\'s something you don\'t know.” ',
+  source: 'Eoin Colfer',
+  citation: 'Artemis Fowl',
+  year: undefined,
+},
+{
+  quote: '“Success is most often achieved by those who don\'t know that failure is inevitable.”',
+  source: 'Coco Chanel',
+  citation: '',
+  year: 1992,
+},
+{
+  quote: '“We either make ourselves miserable, or we make ourselves strong. The amount of work is the same.” ',
+  source: 'Carlos Castaneda',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '“When I was a child my mother said to me, \'If you become a soldier, you\'ll be a general. If you become a monk, you\'ll be the pope.\' Instead I became a painter and wound up as Picasso.”',
+  source: 'Pablo Picasso',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '“A great man is always willing to be little.”',
+  source: 'Ralph Waldo Emerson',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '“Be who you are and say what you feel, because those who mind don\'t matter, and those who matter don\'t mind.”',
+  source: '',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '“You\'ve gotta dance like there\'s nobody watching, Love like you\'ll never be hurt, Sing like there\'s nobody listening, And live like it\'s heaven on earth.” ',
+  source: 'William W. Purkey',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '"Failure will never overtake me if my determination to succeed is strong enough"',
+  source: 'Og Mandino',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '"Life is 10% what happens to you and 90% how you react to it"',
+  source: 'Charles R. Swindoll',
+  citation: '',
+  year: undefined,
+},
+{
+  quote: '"I\'d rather attempt to do something great and fail than to attempt to do nothing and succeed."',
+  source: 'Robert H. Schuller',
+  citation: '',
+  year: undefined,
+},
+]
 
 /***
   Create the `getRandomQuote` function to:
@@ -26,7 +77,14 @@ FSJS project 1 - A Random Quote Generator
      `quotes` array.
 ***/
 
+function getRandomNumber (maxNum) {
+  Math.floor(Math.random() * maxNum)
+}
 
+function getRandomQuote () {
+  return getRandomNumber(10)
+
+}
 
 
 /***
@@ -49,7 +107,7 @@ FSJS project 1 - A Random Quote Generator
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
